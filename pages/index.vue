f3425a34e97cb0e2311b2a48704aaa0a843b692c
andeleuze <katsuya.ando496@gmail.com>
2019-03-13 16:24:17 +0900

    コンテンツ追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,50 @@
 <template>
-<div>
-    Hello World!
-    <div>hoge hoge</div>
-</div>
+  <div class="container">
+    <h2 class="title">My Portfolio</h2>
+
+    <ul class="list">
+      <li class="listItem">
+        <h3>Profile</h3>
+        <p class="listItem--box">
+          2017年某国立大学大学院人間・環境学研究科博士課程を退学。ウェブ広告系の企業にPHPエンジニアとして入社。複数の自社サービスのバックエンド・フロントエンド開発を担当。<br>
+          2018年、オーダースーツブランドを展開する企業に入社。自社サービスECサイトのバックエンド開発を担当。<br>
+          いわゆる文系エンジニア。
+        </p>
+      </li>
+      <li class="listItem">
+        <h3>スキル</h3>
+        <ul>
+          <li>PHP</li>
+          <li>Laravel</li>
+          <li>CakePHP</li>
+          <li>JavaScript</li>
+          <li>Vue.js</li>
+          <li>Nuxt.js</li>
+          <li>HTML5</li>
+          <li>CSS</li>
+        </ul>
+      </li>
+      <li class="listItem">
+        <h3>関連ページ</h3>
+        <ul>
+          <li>
+            <a href="https://github.com/AnDeriens">Github</a>
+          </li>
+          <li>
+            <a href="https://qiita.com/Anderiens">Qiita</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
 </template>
 
+<style scoped>
+.container {
+  width: 800px;
+  margin: 0 auto;
+}
+li {
+  list-style: none;
+}
+</style>
